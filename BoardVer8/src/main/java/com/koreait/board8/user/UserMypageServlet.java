917449cb1829cacc4a7b1f62566acba426a78789
AnWonYoung <ko1529@naver.com>
2021-05-27 16:39:55 +0900

    게시판 만들기(ajax) 업로드 파일 이동 구현

--- BoardVer8/src/main/java/com/koreait/board8/user/UserMypageServlet.java
+++ BoardVer8/src/main/java/com/koreait/board8/user/UserMypageServlet.java
@@ -1,6 +1,8 @@
 package com.koreait.board8.user;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.UUID;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,6 +11,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.koreait.board8.MyUtils;
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 
 @WebServlet("/user/mypage")
 public class UserMypageServlet extends HttpServlet {
@@ -20,7 +24,39 @@ public class UserMypageServlet extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
+		String uploadPath = request.getRealPath("/res/img"); // 해당 메소드는 곧 없어진다는 뜻
+		
+		int maxFileSize = 10_485_760; // 10*1024*1024(10mb)
+		
+		System.out.println("uploadPath : " + uploadPath);
+		
+		MultipartRequest multi = new MultipartRequest(request, uploadPath + "/temp", maxFileSize, "UTF-8", new DefaultFileRenamePolicy());
+		// MultipartRequest 인스턴스 생성(cos.jar의 라이브러리)
+		// 파일을 저장할 디렉토리 지정 (uploadPath)
+		// 첨부파일 최대 용량 설정(bite) / 10KB / 용량 초과 시 예외 발생
+		// new DefaultFileRenamePolicy() 중복 파일 처리(동일한 파일명이 업로드되면 뒤에 숫자 등을 붙여 중복 회피)
+		
+		int loginUserPk = MyUtils.getLoginUserPK(request);
+		
+		String targetFolder = uploadPath + "/user/" + loginUserPk; // 여기서 설정하면서 로그인이 필수
+		
+		// 파일 객체 생성
+		File folder = new File(targetFolder);
+		if(!folder.exists()) { // 폴더가 존재하지 않으면 false
+			folder.mkdirs(); // false일 때 폴더를 생성한다는 뜻(무조건 mkdirs를 사용)
+		}
+		// 업로드가 된 파일명 받아오기
+		String fileNm = multi.getFilesystemName("profileImg");
+		System.out.println("fileNm : " + fileNm);
+		
+		int lastDotIdx = fileNm.lastIndexOf("."); // indexOf만 하면 왼쪽부터 찾음, last는 오른쪽에서부터 찾아서 해당 위치값을 전달
+		String ext = fileNm.substring(lastDotIdx); // 위에서 받은 값을 자르고 jpg라는 확장자만 따오게 됨
+		String newfileNm = UUID.randomUUID().toString() + ext; //UUID를 문자열로 바꾼 다음 아래에서 사용
+		
+		// 위 주소값을 파일로 이동
+		File imgFile = new File(uploadPath + "/temp" + "/" + fileNm); // 위에서 temp안에 들어간 사진을 불러온 것
+		imgFile.renameTo(new File(targetFolder + "/" + newfileNm)); // 안에 이동하려는 파일 주소값을 적어두기(이걸 UUID로 적용)
+																		  // renameTo를 사용해서 원래 위치하고자 하는 폴더에 이동하는 원리
 	}
 
 }
--- BoardVer8/src/main/webapp/WEB-INF/view/user/mypage.jsp
+++ BoardVer8/src/main/webapp/WEB-INF/view/user/mypage.jsp
@@ -1,8 +1,10 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@ taglib uri = "http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
 <link rel="stylesheet" href="/res/css/mypage.css">
 <div>
+<!-- img라는 키 값으로 pageContext에 /res/img/noprofile.jpg를 setAttribute한 것 -->
 	<c:choose>
 		<c:when test="${empty sessionSope.loginUser.profileImg}">
 			<c:set var="img" value="/res/img/noprofile.jpg"/>
@@ -11,17 +13,26 @@
 			<c:set var="img" value="${sessionSope.loginUser.profileImg}"/>
 		</c:otherwise>
 	</c:choose>
+	<!-- accept로 이미지 파일 확장자만 업로드할 수 있도록 설정 / name 값도 중요하니 생각하고 넣기 -->
+	<!-- cos.jar 라이브러리 다운 후 업로드를 여러 개 할 수 있도록 구현하기 -->
 	<div>
-		<form action="mypage" method="post" enctype="">
-			이미지변경 : <input type="file" name="profileImg">
+		<form action="mypage" method="post" enctype="multipart/form-data">
+			이미지변경 : <input type="file" name="profileImg" accept="image/*">
 			<input type="submit" value="이미지 업로드">
 		</form>
 	</div>
 </div>
 
 <div>
+<!--  위에서 setAttribute해줘서 el식으로(img값) 사용이 가능해짐 -->
 	<div><img src="${img}"></div>
 	<div>PK : ${sessionSope.loginUser.iuser}</div>
 	<div>ID : ${sessionSope.loginUser.uid}</div>
 	<div>Name : ${sessionSope.loginUser.unm}</div>
 </div>
+
+<!-- 
+이클립스에서는 사진 파일이 실제로 보이지 않음
+아레 폴더 안에서 업로드를 확인할 수 있음
+D:\JavaBackendClass\JavaBackendClass\JavaBackendClass\WebWorkspace\.metadata\.plugins\org.eclipse.wst.server.core\tmp0\wtpwebapps\BoardVer8\res\img
+-->
